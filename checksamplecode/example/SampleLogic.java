3e9a545502b2305f3691d01b8a7e1a91bb816908
Toshiyuki Imaizumi <evening_glow@khf.biglobe.ne.jp>
2022-05-13 11:59:24 +0900

    ローカル変数が引数の場合に実際に代入されている型を見てチェックするように

--- checksamplecode/example/SampleLogic.java
+++ checksamplecode/example/SampleLogic.java
@@ -1,14 +1,83 @@
 package example;
 
 public class SampleLogic implements Logic{
+	private Responder fieldResponder;
+	
 	@Override
 	public void run() {
-		SimpleResponder responder = new SimpleResponder();
-		Util.save(responder);
-		Util.save(new SimpleResponder());
-		Util.save(new LogicResponder(this));
+		Responder simpleResponder = new SimpleResponder();
+		Util.save(simpleResponder); // NG
+		Responder logicResponder = new LogicResponder(this);
+		Util.save(logicResponder); // OK
+		Responder localResponderByField = fieldResponder;
+		Util.save(localResponderByField); // OK(フィールドは判定対象外)
+		Util.save(fieldResponder); // OK(フィールドは判定対象外)
+		Responder localResponderByMethod = getResponder();
+		Util.save(localResponderByMethod); // OK(メソッドは判定対象外)
+		{
+			Responder r = new SimpleResponder();
+			Util.save(r); // NG
+		}
+		{
+			Responder r = new LogicResponder(this);
+			Util.save(r); // OK
+		}
+		{
+			Responder r;
+			r = new SimpleResponder(); 
+			Util.save(r); // NG
+		}
+		{
+			Responder r;
+			r = new LogicResponder(this); 
+			Util.save(r); // OK
+		}
+		{
+			Responder r = e->{};
+			Util.save(r); // NG
+		}
+		{
+			Responder r = new SimpleResponder() {};
+			Util.save(r); // NG
+		}
+		{
+			Responder r = new LogicResponder(this) {};
+			Util.save(r); // OK
+		}
+		
+		Util.save(getResponder()); // OK(メソッドは判定対象外)
+		Util.save(e->{}); // NG
+		Util.save(new SimpleResponder()); //NG
+		Util.save(new LogicResponder(this)); //OK
+		Util.save(new Responder() { //NG
+			@Override
+			public void callback(String result) {
+				
+			}
+		});
+		Util.save(new LogicResponder(this) { //OK
+			@Override
+			public void callback(String result) {
+				
+			}
+		});
+		new Logic() {
+			@Override
+			public void run() {
+				Util.save(new SimpleResponder()); //NG
+				Util.save(new LogicResponder(this)); //OK
+			}
+		}.run();
 	}
 	
+	public void hoge(Responder responder) {
+		Util.save(responder); //OK(引数の場合は判定対象外)
+	}
+	
+	private Responder getResponder() {
+		return null;
+	}
+
 	// メソッド名が30文字以上の場合のチェック処理のテスト用
 	public void longlonglonglonglonglonglonglonglonglongName() {
 		 
--- src/jp/gr/java_conf/tsyki/codechecker/visitor/ArgumentTypeCheckVisitor.java
+++ src/jp/gr/java_conf/tsyki/codechecker/visitor/ArgumentTypeCheckVisitor.java
@@ -4,12 +4,20 @@ import org.eclipse.core.resources.IMarker;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.Assignment;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.IBinding;
 import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.IVariableBinding;
+import org.eclipse.jdt.core.dom.LambdaExpression;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
 /**
  * 特定のインタフェース(Logic)を実装したクラスで特定の型の引数(Responder)を持つメソッドを呼び出した際に、その実引数が特定の型(LogicResponder)を実装していなければ警告を出すvisitor
@@ -40,15 +48,118 @@ public class ArgumentTypeCheckVisitor extends ASTVisitor {
 			ITypeBinding parameterType = parameterTypes[i];
 			// 引数の型がResponderである場合、実引数の型がLogicResponderかどうかをチェック
 			if (RESPONDER_FQCN.equals(parameterType.getQualifiedName())) {
-				// 匿名クラスの場合はClassInstanceCreation、変数の場合はSimpleNameであり、どちらもExpressionである
+				// 匿名クラスの場合はClassInstanceCreation、変数の場合はSimpleName、ラムダ式の場合はLamdaExpression、メソッドの場合はMethodInvocationであり、いずれもExpressionである
 				Expression arg = (Expression) node.arguments().get(i);
-				ITypeBinding typeBinding = ((Expression) arg).resolveTypeBinding();
-				if (!isImplements(LOGIC_RESPONDER_FQCN, typeBinding)) {
-					createMarker(node);
+				// メソッドの結果が渡されている場合は実際の型の判別ができないため、チェックしない
+				if (arg instanceof MethodInvocation) {
+					continue;
+				}
+				// 実引数が変数かつ、ローカル変数の場合、その変数に代入されている型がLogicResponderであるかチェック
+				// フィールドやメソッドの引数の場合はLogicResponderが渡されるかは不明であるためチェックしない。
+				// ローカル変数の場合でもnewした値を設定するのではなく、メソッドの呼び出し結果を代入する場合はチェックしない
+				if (arg instanceof SimpleName) {
+					SimpleName variable = (SimpleName) arg;
+					IBinding bind = variable.resolveBinding();
+					if (bind instanceof IVariableBinding) {
+						IVariableBinding varBind = (IVariableBinding) bind;
+						// フィールドか引数ならチェックしない
+						if (varBind.isField() || varBind.isParameter()) {
+							continue;
+						}
+						// ローカル変数の場合は親メソッドを取得して代入をチェック
+						MethodDeclaration method = getParentMethod(variable);
+						// 同名のローカル変数でもkeyは以下のように異なる
+						// Lexample/SampleLogic;.run()V#0#r
+						// Lexample/SampleLogic;.run()V#1#r
+						VariableAssignVisitor visitor = new VariableAssignVisitor(bind.getKey());
+						method.accept(visitor);
+						if (visitor.existsNotLogicResponderAssigned()) {
+							createMarker(node);
+						}
+					}
+				} else {
+					// 引数が変数でない場合はその式の型をチェック
+					ITypeBinding typeBinding = ((Expression) arg).resolveTypeBinding();
+					if (!isImplements(LOGIC_RESPONDER_FQCN, typeBinding)) {
+						createMarker(node);
+					}
 				}
 			}
 		}
 		return super.visit(node);
+	}
+
+	// 指定の変数にLogicResponder以外が代入されているかどうかをチェックするvisitor
+	private static class VariableAssignVisitor extends ASTVisitor {
+		private String searchVariableKey;
+
+		private boolean existsNotLogicResponderAssigned;
+
+		public VariableAssignVisitor(String searchVariableKey) {
+			this.searchVariableKey = searchVariableKey;
+		}
+
+		public boolean existsNotLogicResponderAssigned() {
+			return existsNotLogicResponderAssigned;
+		}
+
+		private boolean isNotLogicResponderAssigned(Expression leftSide, Expression rightSide) {
+			if (leftSide instanceof SimpleName) {
+				// 調査対象の変数であるかをチェック
+				IBinding bind = ((SimpleName) leftSide).resolveBinding();
+				if (!searchVariableKey.equals(bind.getKey())) {
+					return false;
+				}
+				// newしている場合はそのクラスの型をチェック
+				if (rightSide instanceof ClassInstanceCreation) {
+					ITypeBinding typeBinding = rightSide.resolveTypeBinding();
+					if (!isImplements(LOGIC_RESPONDER_FQCN, typeBinding)) {
+						return true;
+					}
+				}
+				// ラムダ式の場合はLogicResponderではないので探索終了
+				else if (rightSide instanceof LambdaExpression) {
+					existsNotLogicResponderAssigned = true;
+					return true;
+				}
+				// メソッド、変数の代入の場合はそれ以上見ない
+			}
+			return false;
+		}
+
+		// 変数宣言と同時に代入した場合
+		@Override
+		public boolean visit(VariableDeclarationFragment node) {
+			SimpleName leftSide = node.getName();
+			Expression rightSide = node.getInitializer();
+			if (isNotLogicResponderAssigned(leftSide, rightSide)) {
+				existsNotLogicResponderAssigned = true;
+				return false;
+			}
+			return super.visit(node);
+		}
+
+		@Override
+		public boolean visit(Assignment node) {
+			Expression leftSide = node.getLeftHandSide();
+			Expression rightSide = node.getRightHandSide();
+			if (isNotLogicResponderAssigned(leftSide, rightSide)) {
+				existsNotLogicResponderAssigned = true;
+				return false;
+			}
+			return super.visit(node);
+		}
+	}
+
+	private MethodDeclaration getParentMethod(ASTNode node) {
+		if (node instanceof MethodDeclaration) {
+			return (MethodDeclaration) node;
+		}
+		// クラスの宣言が見つかるまで親を探索
+		if (node.getParent() != null) {
+			return getParentMethod(node.getParent());
+		}
+		return null;
 	}
 
 	private void createMarker(MethodInvocation node) {
@@ -65,7 +176,7 @@ public class ArgumentTypeCheckVisitor extends ASTVisitor {
 		}
 	}
 
-	private boolean isImplements(String fqcn, ITypeBinding binding) {
+	private static boolean isImplements(String fqcn, ITypeBinding binding) {
 		if (binding == null) {
 			return false;
 		}
